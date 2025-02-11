af472ae49ee6416515f2f6c21bd6984a6e053ed5
Emmanuel Obonyo <emmyobonyo@gmail.com>
2025-02-11 16:34:02 +0300

    remove redundant e from onclick method

--- app/ui/calculator/card.tsx
+++ app/ui/calculator/card.tsx
@@ -87,7 +87,7 @@ export default function Card() {
       </div>
       <button
         className="w-full text-white bg-black hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        onClick={(e) => calculateWaterQuantity()}
+        onClick={calculateWaterQuantity}
       >
         Calculate Water Quantity
       </button>
